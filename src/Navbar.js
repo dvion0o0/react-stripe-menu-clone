533c9c0d2448516a83ccf88ec7285cb56e1eb083
Dv <dv@Dvs-MacBook-Pro.local>
2021-01-05 21:39:17 +0530

    stripe menu

---
+++ src/Navbar.js
@@ -0,0 +1,34 @@
+import React from "react";
+import logo from "./images/logo.svg";
+import { FaBars } from "react-icons/fa";
+import { useGlobalContext } from "./context";
+
+const Navbar = () => {
+  const { openSidebar, openSubmenu, closeSubmenu } = useGlobalContext();
+  return (
+    <nav className="nav">
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} className="nav-logo" alt="logo" />
+          <button className="btn toggle-btn" onClick={openSidebar}>
+            <FaBars></FaBars>
+          </button>
+        </div>
+        <ul className="nav-links">
+          <li>
+            <button className="link-btn">products</button>
+          </li>
+          <li>
+            <button className="link-btn">developers</button>
+          </li>
+          <li>
+            <button className="link-btn">company</button>
+          </li>
+        </ul>
+        <button className="btn signin-btn">Sign In</button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/context.js
@@ -0,0 +1,46 @@
+import React, { useState, useContext } from "react";
+import { Children } from "react";
+import sublinks from "./data";
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+
+  const openSubmenu = () => {
+    setIsSubmenuOpen(true);
+  };
+
+  const closeSubmenu = () => {
+    setIsSubmenuOpen(false);
+  };
+  return (
+    <AppContext.Provider
+      value={{
+        isSidebarOpen,
+        isSubmenuOpen,
+        openSidebar,
+        openSubmenu,
+        closeSidebar,
+        closeSubmenu,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
